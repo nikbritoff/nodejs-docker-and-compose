2ac6d1ddb5eae48e6d3541c97b1f16a69d37d722
nikbritoff <britov.nd@gmail.com>
2025-09-29 19:13:30 +0300

    fix: .env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -18,7 +18,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: './.env',
+      envFilePath: '../.env',
       isGlobal: true,
     }),
     TypeOrmModule.forRootAsync({
